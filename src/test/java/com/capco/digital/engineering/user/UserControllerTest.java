1769b3d0e94288a49c11ed3ad074323ecbb30683
Matt Angus <>
2018-06-11 11:04:30 +0100

    Add docker hosts

--- src/test/java/com/capco/digital/engineering/user/UserControllerTest.java
+++ src/test/java/com/capco/digital/engineering/user/UserControllerTest.java
@@ -11,58 +11,62 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.testng.annotations.Test;
 
 import com.capco.digital.engineering.AbstractTest;
 
 public class UserControllerTest extends AbstractTest {
 
-    @InjectMocks
-    private UserController controller = new UserController();
+	@InjectMocks
+	private UserController controller = new UserController();
 
-    @Mock
-    private UserService userService;
+	@Mock
+	private UserService userService;
 
-    @Test
-    public void testGetUsers() {
-        when(userService.findAllUsers()).thenReturn(getUserList());
+	@Mock
+	private KafkaTemplate<String, String> kafkaTemplate;
 
-        ResponseEntity<List<User>> response = controller.getUsers();
-        assertEquals(response.getStatusCode(), HttpStatus.OK);
-        assertEquals(response.getBody().size(), 2);
-        assertNotNull(response.getBody().get(0).getId());
-        assertNotNull(response.getBody().get(0).getFirstname());
-        assertNotNull(response.getBody().get(0).getLastname());
-    }
+	@Test
+	public void testGetUsers() {
+		when(userService.findAllUsers()).thenReturn(getUserList());
 
-    @Test
-    public void testPostUser() {
-        User user = new User();
-        user.setFirstname("Test");
-        user.setLastname("Test");
+		ResponseEntity<List<User>> response = controller.getUsers();
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
+		assertEquals(response.getBody().size(), 2);
+		assertNotNull(response.getBody().get(0).getId());
+		assertNotNull(response.getBody().get(0).getFirstname());
+		assertNotNull(response.getBody().get(0).getLastname());
+	}
 
-        when(userService.insertUser(user)).thenReturn(user);
+	@Test
+	public void testPostUser() {
+		User user = new User();
+		user.setFirstname("Test");
+		user.setLastname("Test");
 
-        ResponseEntity<User> response = controller.addUser(user);
-        assertEquals(response.getStatusCode(), HttpStatus.CREATED);
-    }
+		when(userService.insertUser(user)).thenReturn(user);
 
-    private List<User> getUserList() {
-        List<User> users = new ArrayList<>();
-        User user1 = new User();
-        user1.setFirstname("Dave");
-        user1.setLastname("Smith");
-        user1.setId("1");
+		ResponseEntity<User> response = controller.addUser(user);
+		assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+	}
 
-        User user2 = new User();
-        user2.setFirstname("John");
-        user2.setLastname("Smith");
-        user2.setId("2");
+	private List<User> getUserList() {
+		List<User> users = new ArrayList<>();
+		User user1 = new User();
+		user1.setFirstname("Dave");
+		user1.setLastname("Smith");
+		user1.setId("1");
 
-        users.add(user1);
-        users.add(user2);
+		User user2 = new User();
+		user2.setFirstname("John");
+		user2.setLastname("Smith");
+		user2.setId("2");
 
-        return users;
-    }
+		users.add(user1);
+		users.add(user2);
+
+		return users;
+	}
 
 }
