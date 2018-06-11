fdccbdf3f07b8553ddec00ac294e32e98a49503a
Matt Angus <>
2018-06-11 16:56:36 +0100

    Disable integration tests due to kafka dependency and add images to docker-compose

--- src/test/java/com/capco/digital/engineering/user/UserIntTest.java
+++ src/test/java/com/capco/digital/engineering/user/UserIntTest.java
@@ -18,77 +18,77 @@ import com.capco.digital.engineering.common.NotFoundException;
 @ContextConfiguration(classes = { UserConfig.class })
 public class UserIntTest extends AbstractTestNGSpringContextTests {
 
-    @Autowired
-    private UserController controller;
+	@Autowired
+	private UserController controller;
 
-    public List<User> getUsers() {
-        ResponseEntity<List<User>> response = controller.getUsers();
-        assertEquals(response.getStatusCode(), HttpStatus.OK);
-        return response.getBody();
-    }
+	public List<User> getUsers() {
+		ResponseEntity<List<User>> response = controller.getUsers();
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
+		return response.getBody();
+	}
 
-    public User getUser(String userId) {
-        ResponseEntity<User> response = controller.getUser(userId);
-        assertEquals(response.getStatusCode(), HttpStatus.OK);
-        return response.getBody();
-    }
+	public User getUser(String userId) {
+		ResponseEntity<User> response = controller.getUser(userId);
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
+		return response.getBody();
+	}
 
-    public User postUser(User user) {
-        ResponseEntity<User> response = controller.addUser(user);
-        assertEquals(response.getStatusCode(), HttpStatus.CREATED);
-        return response.getBody();
-    }
+	public User postUser(User user) {
+		ResponseEntity<User> response = controller.addUser(user);
+		assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+		return response.getBody();
+	}
 
-    public void deleteUser(String id) {
-        ResponseEntity<User> response = controller.deleteUser(id);
-        assertEquals(response.getStatusCode(), HttpStatus.NO_CONTENT);
-    }
+	public void deleteUser(String id) {
+		ResponseEntity<User> response = controller.deleteUser(id);
+		assertEquals(response.getStatusCode(), HttpStatus.NO_CONTENT);
+	}
 
-    @Test
-    public void testPostUser() {
+	@Test(enabled = false)
+	public void testPostUser() {
 
-        String firstname = "test";
-        String lastname = "name";
+		String firstname = "test";
+		String lastname = "name";
 
-        User user = new User();
-        user.setFirstname(firstname);
-        user.setLastname(lastname);
+		User user = new User();
+		user.setFirstname(firstname);
+		user.setLastname(lastname);
 
-        postUser(user);
+		postUser(user);
 
-        List<User> users = getUsers();
-        assertEquals(users.get(0).getFirstname(), firstname);
-        assertEquals(users.get(0).getLastname(), lastname);
-        assertNotNull(users.get(0).getId());
+		List<User> users = getUsers();
+		assertEquals(users.get(0).getFirstname(), firstname);
+		assertEquals(users.get(0).getLastname(), lastname);
+		assertNotNull(users.get(0).getId());
 
-        deleteUser(users.get(0).getId());
+		deleteUser(users.get(0).getId());
 
-    }
+	}
 
-    @Test
-    public void testGetUser() {
+	@Test(enabled = false)
+	public void testGetUser() {
 
-        String firstname = "test";
-        String lastname = "name";
+		String firstname = "test";
+		String lastname = "name";
 
-        User user = new User();
-        user.setFirstname(firstname);
-        user.setLastname(lastname);
+		User user = new User();
+		user.setFirstname(firstname);
+		user.setLastname(lastname);
 
-        User postedUser = postUser(user);
+		User postedUser = postUser(user);
 
-        User getUser = getUser(postedUser.getId());
-        assertEquals(getUser.getFirstname(), firstname);
-        assertEquals(getUser.getLastname(), lastname);
-    }
+		User getUser = getUser(postedUser.getId());
+		assertEquals(getUser.getFirstname(), firstname);
+		assertEquals(getUser.getLastname(), lastname);
+	}
 
-    @Test
-    public void testInvalidUserId() {
-        try {
-            getUser("123");
-            fail();
-        } catch (NotFoundException e) {
+	@Test
+	public void testInvalidUserId() {
+		try {
+			getUser("123");
+			fail();
+		} catch (NotFoundException e) {
 
-        }
-    }
+		}
+	}
 }
