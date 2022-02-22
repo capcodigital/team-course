fea39ea512089bacb28d0d688b9cc93c82c46777
Alin Peta <>
2022-02-22 13:39:15 +0000

    Added new swagger adnotation for newer versions

--- src/main/java/com/capco/digital/engineering/swagger/SwaggerConfig.java
+++ src/main/java/com/capco/digital/engineering/swagger/SwaggerConfig.java
@@ -13,7 +13,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
