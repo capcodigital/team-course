d7ac86fa0304e2aaa60b89ab7190e277b4770643
Stan <you@example.com>
2022-04-06 12:32:04 +0100

    fix: sign commit

--- src/main/java/com/capco/digital/engineering/swagger/SwaggerConfig.java
+++ src/main/java/com/capco/digital/engineering/swagger/SwaggerConfig.java
@@ -26,7 +26,6 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
