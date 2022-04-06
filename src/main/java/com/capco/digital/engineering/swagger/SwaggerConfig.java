691d47175abe02a1605d17bd9f615da7242f2d42
Stan <stan.ivanov@capco.com>
2022-04-06 12:38:40 +0100

    fix: sign commit and fix spaces

--- src/main/java/com/capco/digital/engineering/swagger/SwaggerConfig.java
+++ src/main/java/com/capco/digital/engineering/swagger/SwaggerConfig.java
@@ -26,6 +26,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
